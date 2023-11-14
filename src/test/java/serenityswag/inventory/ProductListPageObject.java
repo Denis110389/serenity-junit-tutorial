2391c915f4ac3b1235a49f314c613db4b552cc9f
dmorento <denis.morenotorres.st@everis.nttdata.com>
2023-11-14 12:35:55 +0100

    create ProductListPageObject

---
+++ src/test/java/serenityswag/inventory/ProductListPageObject.java
@@ -0,0 +1,11 @@
+package serenityswag.inventory;
+
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.pages.PageObject;
+import java.util.List;
+
+public class ProductListPageObject extends PageObject {
+    public List<String> titles(){
+        return $$(By.className("inventory_item_name")).textContents();
+    }
+}
